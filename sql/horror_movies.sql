ed91b37589f91a0ff32cf272d26ebac9bc1a09d2
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-10-15 17:02:57 -0400

    changing sql code a bit

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,7 +1,7 @@
 SELECT id AS 'Movie_ID', name AS 'Movie_Title',  imdb_rating AS 'Rating'
 FROM movies
 WHERE genre = 'horror' 
-  AND year <= 1985 
+AND year <= 1985 
 ORDER BY imdb_rating DESC
 LIMIT 3
 ;
